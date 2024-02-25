b9d74be1dfb2679812bdff191892258118f141ec
Yuan Chen <cysbc1999@gmail.com>
2024-02-25 17:09:18 -0500

    Initial Commit

---
+++ VisionTrader/VisionTraderApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionTraderApp.swift
+//  VisionTrader
+//
+//  Created by Yuan Chen on 2/25/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionTraderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
